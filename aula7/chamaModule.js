8ca217ded42f236916d6e2118820630eb538b78c
Lucas Rockenbach <105431330+LucasRockenbach@users.noreply.github.com>
2023-04-11 21:34:46 -0300

    11/04

---
+++ aula7/chamaModule.js
@@ -0,0 +1,12 @@
+const modulo = require ('./index.js')
+
+console.log(`entrou no chama modulo`)
+console.log(`valor da minha varialvel ${modulo.minhaVariavel}`)
+modulo.minhaFuncao()
+
+let value1 = 255
+let value2 = 88
+
+//let valorTotal = modulo.somarValores (value1, value2)
+//console.log(`valor somado ${valorTotal}`)
+
