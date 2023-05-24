659672009f5a5e1c9b1ea9994487f2dd77c9b3c9
Lucas Rockenbach <105431330+LucasRockenbach@users.noreply.github.com>
2023-05-23 21:14:55 -0300

    23/05

---
+++ index - Copia.js
@@ -0,0 +1,18 @@
+let http = require('http');
+
+let servidor = http.createServer(function(req, res){
+	let endereco = req.url;
+	
+	if(endereco == "/listar"){
+		res.end("<html><head><title>Pagina Listar</title></head>"+
+		"<body><h1>Pagina de listagem!</h1></body></html>");	
+	}else if(endereco == '/cadastrar'){
+		res.end("<html><head><title>Pagina Cadastrar</title></head>"+
+		"<body><h1>Pagina de cadastro!</h1></body></html>");
+	}else{
+		res.end("<html><head><title>Pagina inicial</title></head>"+
+		"<body><h1>Pagina inicial!</h1></body></html>");		
+	}
+});
+
+servidor.listen(8000);
--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-let http = require('http')
-let express = require('express')
-let app = express()
+let express = require('express');
 
-app.set('views engine' , 'ejs')
+let app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('views/static'));
 
-app.get('/cadastrar', function(req, res){
-    res.render('acoes/index')
-})
+let routeHome = require('./route/home')
+routeHome.get(app)
 
-/*  request solicitação do cliente  
-    response resposta do servidor ao cliente    
+let routeCadastrar = require('./route/cadastrar')
+routeCadastrar.get(app)
+routeCadastrar.p(app)
+routeCadastrar.u(app)
 
-let servidor = http.createServer(function(req, res) {
-    let endereco = req.url
-    console.log("url" + endereco)
-    if(endereco == "/listar"){
-        res.end('<HTML> <head> <title> Pagina de cadastro </title> </head> <body> <h1> Pagina de listagem </h1></body> </HTML>')
-    }else if(endereco == "/cadastrar"){
-        res.end('<HTML> <head> <title> Pagina inicial </title> </head> <body> <h1> Pagina de cadastro </h1></body> </HTML>')
-    }else{
-        res.end('<HTML> <head> <title> Pagina inicial </title> </head> <body> <h1> Pagina inicial </h1></body> </HTML>')
-    }
+let routeListar = require('./route/listar')
+routeListar.get(app)
 
 
-})*/
 
-servidor.listen(8000)
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/listar_cliente', function(req, res){
+	res.render('pages/listar_clientes');
+});
+
+app.get('/cadastrar_cliente', function(req, res){
+	res.render('pages/cadastrar_cliente');
+});
+
+app.listen(8000, function(req, res){
+	console.log('Servidor rodando na porta 8000');
+});
